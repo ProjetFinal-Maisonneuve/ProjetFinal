702c109a01137b7da1b2b357e5c1fd1b73a0be20
Philippe Cossette <philcossette@hotmail.com>
2025-11-18 15:23:10 -0500

    Ajout du script pour toggle les actions de cellier comme modifier et supprimer

--- resources/views/components/cellar-box.blade.php
+++ resources/views/components/cellar-box.blade.php
@@ -1,5 +1,5 @@
 @props(['name'=>'Cellier', 'amount' => '0'])
-<a class="p-3 bg-card rounded-lg flex justify-between shadow-md border-border-base border hover:shadow-sm hover:bg-card-hover transition-all duration-300" href="#">
+<a  class="cellar-box p-3 bg-card rounded-lg flex justify-between shadow-md border-border-base border hover:shadow-sm hover:bg-card-hover transition-all duration-300" href="#">
    <div flex flex-col gap-1>
     <h2 class="text-2xl font-semibold">{{ $name }}</h2>
    @if ($amount > 1)
@@ -8,7 +8,7 @@
        <p class="text-gray-600">{{ $amount }} Bouteille</p>
    @endif
    </div>
-   <div class="flex gap-2 justify-center items-center">
+   <div class="cellar-action-btns flex gap-2 justify-center items-center hidden">
     <x-edit-btn />
     <x-delete-btn />
    </div>
--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -2,6 +2,6 @@
 <header class="mt-header flex flex-wrap justify-between">
     <h1 class="text-3xl font-bold font-heading text-heading">{{ $title }}</h1>
     @if ($actionBtn === true)
-        <x-setting-btn />
+        <x-setting-btn id="setting-btn"/>
     @endif
 </header>
